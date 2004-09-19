e79d7f970d47ecac7022e463010c201748c0dd0d
league <league@contrapunctus.net>
2004-09-19 00:35:50 -0400

    Have a working system for SML transcripts in TeXinfo.
    darcs-hash:20040919043550-2df73-bf484ab41e9a0c00f9f9a08d68c33c2c6fa7947a.gz

---
+++ doc/transcribe.pl
@@ -0,0 +1,60 @@
+#! /usr/bin/perl
+use strict;
+use Expect;
+
+my $cmd = shift @ARGV;
+
+$Expect::Log_Stdout = 0;
+my $ml = new Expect;
+$ml->raw_pty(1);
+$ml->spawn($cmd, "qcheck.cm")
+    or die "Cannot spawn sml: $!\n";
+$ml->expect(undef, '-re', '^- ');
+
+my $line = 0;
+while(<>)
+{
+    print "\@c THIS FILE IS AUTO-GENERATED; DO NOT EDIT!\n"
+        if $line++ == 1;
+    if(/^\@transcript/) 
+    {
+        my $verbosity = 2;
+        $verbosity = 1 if /quiet/;  # show input only, ignore output
+        $verbosity = 0 if /omit/;   # omit entire block, just pipe to ML
+        print "\@example\n" if $verbosity > 0;
+        while(<>)
+        {
+            last if /^\@end transcript/;
+            print $_ if $verbosity > 0;
+            $ml->send($_);
+            $ml->expect(undef, '-re', '^[=-] ');
+            render_output($ml->before()) if $verbosity > 1;
+        }
+        $ml->send(";\n");
+        $ml->expect(undef, '-re', '^- ');
+        render_output($ml->before()) if $verbosity > 1;
+        print "\@end example\n" if $verbosity > 0;
+    }
+    else { print; }
+}
+$ml->send("OS.Process.exit 0 : unit;\n");
+$ml->soft_close();
+exit 0;
+
+sub render_output {
+    my($chunk) = @_;
+    foreach my $line (split '\n', protect_texi($chunk)) {
+        print " \@print{} \@i{$line}\n";
+    }
+}
+
+sub protect_texi {
+    my($text) = @_;
+    $text =~ s/\@/\@\@/g;
+    $text =~ s/\{/\@\{/g;
+    $text =~ s/\}/\@\}/g;
+    ## This has nothing to do with texinfo,
+    ## but we need to remove lines "erased" by \r
+    $text =~ s/[^\r\n]*\r//g;
+    return $text;
+}
--- makefile
+++ makefile
@@ -1,21 +1,29 @@
-
 define each-ml
   for m in nj moscow mlton; do $(MAKE) -f Makefile.$$m $@; done
 endef
 
+DOCS:=$(addprefix doc/qcheck., info pdf)
+
 default: 
-all:
+all: docs
 	$(each-ml)
 test:
 	$(each-ml)
+docs: $(DOCS)
 
-doc: README.html
+$(DOCS): doc/qcheck-ver.texi doc/qcheck.texi
+
+%.texi: %.texin doc/transcribe.pl
+	$(PERL) doc/transcribe.pl $(SML) $< >$@
+
+%.pdf: %.texi
+	$(TEXI2DVI) -p -o $@ $<
 
 %.html: % Makefile
 	tail +2 $< | txt2html -8 +l | recode -d utf8..h3 >$@
 	tidy -asxml -q -m $@
 
-tar: realclean
+tar: clean
 	cd .. && rm -rf qcheck-$(VERSION) \
 	      && cp -r qcheck qcheck-$(VERSION) \
 	      && tar cvzf qcheck-$(VERSION).tgz \
@@ -32,6 +40,7 @@ master.mostlyclean:
 	$(RM) compat/*/*~ tests/data/*~ doc/*~ *~
 master.clean: master.mostlyclean
 master.realclean: master.clean
-	$(RM) src/qcheck-ver.sml
+	$(RM) src/qcheck-ver.sml 
+	$(RM) $(DOCS) doc/qcheck.texi doc/qcheck-ver.texi
 
 include Makefile.version
